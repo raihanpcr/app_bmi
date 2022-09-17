d885f07d1d4b527167611eeaa53a715109954da0
raihanpcr <52515421+raihanpcr@users.noreply.github.com>
2022-09-17 21:36:06 +0700

    Fourth Commit - Card Gender

--- lib/bmi_data_screen.dart
+++ lib/bmi_data_screen.dart
@@ -15,13 +15,54 @@ class _BmiDataScreenState extends State<BmiDataScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff0A0E21),
         appBar: AppBar(
           title: const Text("BMI Calculator"),
         ),
         body: Column(children: [
           Expanded(
               child: Container(
-            color: Colors.red,
+            child: Row(children: [
+              Expanded(
+                  child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff272A4E),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                margin: EdgeInsets.all(15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.male,
+                      size: 80,
+                    ),
+                    Text(
+                      "Male",
+                      style: TextStyle(fontSize: 18, color: Color(0xff8D8E98)),
+                    ),
+                  ],
+                ),
+              )),
+              Expanded(
+                  child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff272A4E),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                margin: EdgeInsets.all(15),
+                    child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                    Icon(Icons.female, size: 80),
+                    Text(
+                      "Female",
+                      style: TextStyle(fontSize: 18, color: Color(0xff8D8E98)),
+                    ),
+                ],
+              ),
+                  ))
+            ]),
           )),
           Expanded(
               child: Container(
